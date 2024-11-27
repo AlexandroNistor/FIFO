35644b032a3be3e7e94590a473f80faae4a4f6cd
AlexandroNistor <sebastian.alex220@gmail.com>
2024-11-27 19:48:56 +0100

    add change 2

--- example.py
+++ example.py
@@ -1,13 +1,25 @@
 OK_RESULT = 'yes'
 FATAL_RESULT = 'no'
+PEPE = 9
+PLAYERS = []
 
-def check_if_aprove_or_not() -> str:
+def check_if_approve_or_not() -> str:
+    result = input('¿Eres del Madrid? ')
 
-    result = input('¿Eres del Madrid?')
+    if isinstance(result, str):
+        if result.lower() == 'yes':
+            return "Aprobado"
+        for i in range(0, 15):
+            r = input('¿Estás seguro? ')
+            if isinstance(r, str) and r == 'yes':
+                return "SUSPENDIDO"
+    return "SUSPENDIDO"
 
-    if isinstance(result,str):
-        if result.lower() =='yes':
-            return 'Aprobado'
-        return 'SUSPENSO'
+def add_players_to_list():
+    while len(PLAYERS) < 11:
+        r = input('Dime un jugador del madrid: ')
+        if isinstance(r, str):
+            PLAYERS.append(r)
 
-check_if_aprove_or_not()
+check_if_approve_or_not()
+add_players_to_list()
